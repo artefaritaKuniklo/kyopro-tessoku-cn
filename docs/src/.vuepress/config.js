360c22238bda00f455acfcb644743d271d664e60
NekoM200 <lychees67@gmail.com>
2022-09-21 16:03:39 +0800

    Add sidebar

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -46,17 +46,93 @@ module.exports = {
         link: '/第 2 章 部分和',
       },      
     ],
-    sidebar: {
-      '/第 1 章 算法与计算复杂性/': [
+    sidebar:
+       [
         {
           title: '第 1 章 算法与计算复杂性',
           collapsable: false,
           children: [
-            '',
+            '/第 1 章 算法与计算复杂性/',
+          ]
+        },
+        {
+          title: '第 2 章 部分和',
+          collapsable: false,
+          children: [
+            '/第 2 章 部分和/',
+          ]
+        },
+        {
+          title: '第 3 章 二分搜索',
+          collapsable: false,
+          children: [
+            '/第 3 章 二分搜索/',
+          ]
+        },
+        {
+          title: '第 4 章 动态规划',
+          collapsable: false,
+          children: [
+            '/第 4 章 动态规划/',
+          ]
+        },
+        {
+          title: '第 5 章 数学问题',
+          collapsable: false,
+          children: [
+            '/第 5 章 数学问题/',
+          ]
+        },
+        {
+          title: '第 6 章 考察法',
+          collapsable: false,
+          children: [
+            '/第 6 章 考察法/',
+          ]
+        },
+        {
+          title: '第 7 章 启发式方法',
+          collapsable: false,
+          children: [
+            '/第 7 章 启发式方法/',
+          ]
+        },
+        {
+          title: '第 8 章 数据结构与询问处理',
+          collapsable: false,
+          children: [
+            '/第 8 章 数据结构与询问处理/',
+          ]
+        },
+        {
+          title: '第 9 章 图论算法',
+          collapsable: false,
+          children: [
+            '/第 9 章 图论算法/',
+          ]
+        },
+        {
+          title: '第 10 章 综合问题',
+          collapsable: false,
+          children: [
+            '/第 10 章 综合问题/',
+          ]
+        },
+        {
+          title: '序章 算法竞赛简介',
+          collapsable: false,
+          children: [
+            '/序章 算法竞赛简介/',
+          ]
+        },
+        {
+          title: '终章 如何进一步学习',
+          collapsable: false,
+          children: [
+            '/终章 如何进一步学习/',
           ]
         }
-      ],
-    }
+      ]
   },
 
   /**
